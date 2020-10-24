de8821f09c9f6041f2dbe637dd95f998c275a71e
Ijaj Mulani <ijajmulani786@gmail.com>
2020-10-24 12:18:34 +0530

    Build files

---
+++ build/precache-manifest.0c9c015f0852ec419cff7de8748636fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc66424c3cab13bb02196909aabd9a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cd1d301081b8ccf6825",
+    "url": "/static/css/main.a24a5c87.chunk.css"
+  },
+  {
+    "revision": "602d7c449162ca25d9a2",
+    "url": "/static/js/2.5a800829.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.5a800829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd1d301081b8ccf6825",
+    "url": "/static/js/main.d50e98f9.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
